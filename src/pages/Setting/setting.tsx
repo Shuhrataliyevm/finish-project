ad7b43212c41c6f52ff31a05ae82d412438e80fa
Muhammadazim <shuhrataliyevm@gmail.com>
2025-04-06 19:08:11 +0500

    setting

--- src/pages/Setting/setting.tsx
+++ src/pages/Setting/setting.tsx
@@ -1,11 +1,11 @@
-import { navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const Setting = () => {
     const navigate = useNavigate();
     return (
         <div>
             <button onClick={() => navigate("/profile")}>
-                layout
+                Layout
             </button>
         </div>
     )
